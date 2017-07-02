367e5e3228e1d0854eb7f1adfb1e6f0abc7d3727
Jhonatan Mattana <jhonatan-mattana@outlook.com>
2017-07-02 03:02:23 -0300

    Criando método de salvar e buscar itens na lista de Produtos

--- src/main/java/br/univel/ProdutoDao.java
+++ src/main/java/br/univel/ProdutoDao.java
@@ -1,5 +1,10 @@
 package br.univel;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,8 +13,51 @@ import java.util.List;
  */
 public class ProdutoDao {
 
-	public void salvar(List<Produto> minhaLista) {
-		
-	}
+	private Connection con;
 
+	public void selectFrom (){
+		con = Conexao.getInstance().getConnection();
+		
+		List<Produto> listaP = new ArrayList<Produto>();
+		
+		String selectFrom = "SELECT * FROM produto";
+		
+		PreparedStatement ps;
+		try {
+			ps = con.prepareStatement(selectFrom);
+			
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				Produto p = new Produto();
+				p.setId(rs.getLong(1));
+				p.setDescricao(rs.getString(2));
+				p.setValorDolar(rs.getBigDecimal(3));
+				listaP.add(p);
+			}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void salvar(List<Produto> minhaLista) {
+		con = Conexao.getInstance().getConnection();
+
+		String insertInto = "INSERT INTO produto (id,descricao,valorDolar)"
+				+ "VALUES (?,?,?)";
+		
+		try {
+			PreparedStatement ps = con.prepareStatement(insertInto);
+			
+			for (Produto p : minhaLista) {
+				ps.setLong(1, p.getId());
+				ps.setString(2, p.getDescricao());
+				ps.setBigDecimal(3, p.getValorDolar());
+				
+				ps.execute();
+			}
+		} catch (SQLException e) {
+			System.out.println("Erro no INSERT INTO");
+		}
+	}
 }
